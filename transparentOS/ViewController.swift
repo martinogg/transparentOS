f9ee0d39bcbfeef57862f4d5c04f89cbcedad7b3
martin ogg <>
2019-07-24 16:38:34 +0100

    first commit

---
+++ transparentOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  transparentOS
+//
+//  Created by martin ogg on 24/07/2019.
+//  Copyright © 2019 martin ogg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
